6cf15ab37a7dcb1be65826fcf60537b3bd0239a1
SMK <smktech36@gmail.com>
2021-04-15 02:19:13 +0530

    Initial commit-React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+import React, { useRef, useState } from 'react'
 import './App.css';
 
 function App() {
+  var mainSwitch = useRef()
+
+  //the main var to store the switch state
+  const [_switchState, changeState] = useState(false)
+
+  var blshTxt = useRef()
+  var ssTxt = useRef()
+
+
+  // function changeState() {
+  //   if (mainSwitch != undefined) {
+  //     _switchState = mainSwitch.current.checked
+  //   }
+  //   if (ssTxt.current != undefined && blshTxt.current != undefined) {
+  //     _switchState ? ssTxt.current.value = "sdf" : blshTxt.current.value = "sdfsd"
+  //   }
+
+  //   if (this != undefined) {
+  //     console.log(this.ssTxt)
+  //   }
+
+  // }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <label style={{ color: _switchState ? "grey" : 'rgb(58, 135, 236)' }}>Buy Low Sell High</label>
+      <div class="toggle-switch" tabindex="0">
+        <input type="checkbox" ref={mainSwitch} onChange={() => changeState(!_switchState)} name="my_checkbox" value="yes" id="checkbox-id" />
+        <label for="checkbox-id">
+          <div class="area" aria-hidden="true">
+            <div class="background">
+              <div class="handle"></div>
+            </div>
+          </div>
+        </label>
+      </div>
+      <label style={{ color: !_switchState ? "grey" : 'rgb(58, 135, 236)' }}>Short Sell</label>
+
+    </>
   );
 }
 
